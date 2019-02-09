ae85740ea86d19e65aac249d63fca36df43fa491
pacmanmati <pacmanmati@gmail.com>
2019-02-09 22:29:38 +0000

    write to file

--- lze.py
+++ lze.py
@@ -1,13 +1,17 @@
 from bitarray import bitarray
 import sys
-import time
 
-# store file in a string
+# read file to encode and return as a bitarray
 def read_file(name):
-    e = bitarray()
+    ba = bitarray()
     with open(name, 'rb') as f:
-        e.fromfile(f)
-    return e
+        ba.fromfile(f)
+    return ba
+
+# write encoded bitarray to file
+def write_file(ba, name="encoded.bin"):
+    with open(name, 'wb') as f:
+        ba.tofile(f)
 
 # returns a binary tuple:
 # d - distance to start of prefix (int),
@@ -21,27 +25,10 @@ def tuple(d, l, c):
     ba += de + le + ce
     return ba
 
-# slower methods
-
-# def tuple_alt_alt(d, l, c):
-#     ba = bitarray()
-#     de = "{0:b}".format(d).zfill(8)
-#     le = "{0:b}".format(l).zfill(8)
-#     ce = "{0:b}".format(ord(c)).zfill(8)
-#     ba += de + le + ce
-#     return ba
-
-# def tuple_alt(d, l, c):
-#     ba = bitarray()
-#     de = format(d, '08b')
-#     le = format(l, '08b')
-#     ce = format(ord(c), '08b')
-#     ba += de + le + ce
-#     return ba
-
 # lz77 encoder on 'f' using a sliding window of length 'W', and a lookahead buffer 'L'
 def encode(W, L, f): # one byte per int
     print(f)
+    encoded = bitarray()
     for i in range(len(f)):
         bit = f[i]
         window_index = bit - W if i - W >= 0 else 0
@@ -49,34 +36,19 @@ def encode(W, L, f): # one byte per int
         window = f[window_index:i] 
         look_ahead = f[i+1:look_ahead_index+1] # excludes the current bit
         length = 0
-        while pattern in window:
-            length += 1
+        # while pattern in window:
+        #     length += 1
+    return encoded
         
-# missing file name
+# if missing file name
 if len(sys.argv) < 2:
-    print("usage: python lze.py [file_to_encode]")
+    print("usage: python lze.py [file_to_encode] (write_file)")
 
 filename = sys.argv[1]
-#encode(1, 1, read_file(filename))
+ba = encode(1, 1, read_file(filename))
 
-# print(tuple(5,5,'a'))
-# print(tuple_alt(5,5,'a'))
-# print(tuple_alt_alt(5,5,'a'))
-
-# speed test
-def speed_test(NUM_ITER):
-    start = time.time()
-    for i in range(NUM_ITER):
-        tuple(i,i,'a')
-
-    t1 = time.time() - start
-    start = time.time()
-    for i in range(NUM_ITER):
-        tuple_alt(i,i,'a')
-    t2 = time.time() - start
-    start = time.time()
-    for i in range(NUM_ITER):
-        tuple_alt_alt(i,i,'a')
-    t3 = time.time() - start
-    print("t1: {}, t2: {}, t3: {}".format(t1,t2,t3))
-speed_test(100000)
+# save as specified file or default
+if len(sys.argv) > 2:
+    write_file(ba, sys.argv[2])
+else:
+    write_file(ba)
